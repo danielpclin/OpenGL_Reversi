62314fa4ad3ea2245618ddf941207c96d77e0143
林柏臣 <daniel.pclin@gmail.com>
2020-06-23 15:56:37 +0800

    added triangle to opengl window
    Signed-off-by: 林柏臣 <daniel.pclin@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 int main()
@@ -9,7 +11,7 @@ int main()
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
     if (!window)
     {
         glfwTerminate();
@@ -19,11 +21,22 @@ int main()
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    if(glewInit() != GLEW_OK)
+        std::cout <<"GLEW Error" << std::endl;
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
